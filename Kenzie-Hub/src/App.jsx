c65131589097aacfc4ea298cf63466cc9291fc9f
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-02-15 11:33:56 -0300

    start of project

---
+++ Kenzie-Hub/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>123</p>
+    </div>
+  );
+}
+
+export default App;
