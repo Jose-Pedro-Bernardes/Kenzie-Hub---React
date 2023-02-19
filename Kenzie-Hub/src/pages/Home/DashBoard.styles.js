84395f310e3e4b7aca84ed8dfe306bf8e45c65e8
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-02-19 15:21:11 -0300

    Home Page - Start, create render function to name

---
+++ Kenzie-Hub/src/pages/Home/DashBoard.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const container = styled.div`
+  width: 100%;
+
+  height: 100%;
+
+  header {
+    width: 48.75rem;
+  }
+
+  main {
+    width: 48.75rem;
+    .sectionTop {
+    }
+  }
+`;
