fbb07ef9e69b884f8e3240e8a05806da6a64b638
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-02-19 23:24:38 -0300

    define header register page

--- Kenzie-Hub/src/pages/RegisterPage/index.jsx
+++ Kenzie-Hub/src/pages/RegisterPage/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Container } from "./register.styles.js";
+import logo from "../../assets/logoDesk.svg";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
+  const navigate = useNavigate();
   return (
     <>
-      <div className="container"></div>
+      <Container>
+        <section>
+          <figure>
+            <img src={logo} alt="Logo Kenzie Hub" />
+          </figure>
+          <Link to={"/"}>Voltar</Link>
+        </section>
+      </Container>
     </>
   );
 }
---
+++ Kenzie-Hub/src/pages/RegisterPage/register.styles.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  font-family: "Inter", sans-serif;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+
+  gap: 30px;
+
+  section {
+    width: 23.063rem;
+    max-width: 95%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      text-decoration: none;
+      width: 4.188rem;
+      height: 2.507rem;
+
+      font-size: var(--text-body);
+      font-weight: 600;
+
+      background-color: var(--grey-3);
+      color: var(--grey-0);
+
+      border-radius: 4px;
+      border: none;
+
+      cursor: pointer;
+      transition: 0.5s;
+
+      @media (min-width: 1100px) {
+        :hover {
+          background-color: var(--grey-2);
+        }
+      }
+    }
+  }
+`;
